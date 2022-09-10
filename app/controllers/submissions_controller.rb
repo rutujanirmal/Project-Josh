ccfe1abe205785cba6f097a6daccbfe0c44888ac
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-10 20:55:47 +0530

    Fnc-To fetch the submissions assigned to a particular student

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -18,10 +18,22 @@ class SubmissionsController < ApplicationController
     end
   end
 
+  def mysubmissions
+    @student = Student.find(params[:id])
+    @submission = Submission.select("id","subject","batch","details","deadline","teacher_id").where("batch": @student.batch)
+    if @submission.present?
+      render status: 200, json: @submission
+    elsif @submission.empty?
+      render status: 200, json: {msg: "You have no pending assignments"}
+    else
+      render status: 400, json: {error: "Error in fetching data"}
+    end
+  end
+
   private
 
   def submission_params
     params.require(:submission).permit(:subject,:batch,:details,:deadline,:teacher_id)
   end
-  
+
 end
