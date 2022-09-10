ae8ac72221c4ec4534d9ac4432f0d02f70821c1b
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-10 20:02:34 +0530

    Overwrite index to get all the teachers deatils

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,12 @@
 class TeachersController < ApplicationController
+
+  def index
+    @teachers = Teacher.select("id","teacher_name","email")
+    if @teachers.present?
+      render status: 200, json: @teachers
+    else
+      render status: 200, json: {msg: "No teachers data present. Use /create_teacher"}
+    end
+  end
+
 end
