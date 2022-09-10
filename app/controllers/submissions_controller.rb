2b8afb655758ff6fa37a8298a2ffc44c049d3423
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-10 20:33:48 +0530

    Overwrite index to get all the submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,12 @@
 class SubmissionsController < ApplicationController
+
+  def index
+    @submission = Submission.select("id","subject","batch","details","deadline","teacher_id")
+    if @submission.present?
+      render status: 200, json: @submission
+    else
+      render status: 200, json: {msg: "No submission data present. Use /create_submission"}
+    end
+  end
+  
 end
