307998786529523ec946beda82483970f574807e
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-10 19:47:26 +0530

    Overwrite index to get all the students details

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,12 @@
 class StudentsController < ApplicationController
+  
+  def index
+    @students = Student.select("id","student_name","year","roll_no","batch")
+    if @students.present?
+      render status: 200, json: @students
+    else
+      render status: 200, json: {msg: "No students data present. Use /create_student"}
+    end
+  end
+  
 end
