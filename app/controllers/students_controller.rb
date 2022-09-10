922a8509406078f16f126c691cc1f2aaac8e4758
Rutuja Nirmal <64598190+rutujanirmal@users.noreply.github.com>
2022-09-10 19:56:44 +0530

    Controller/students (#6)
    * Generate Student Controller
    
    * Overwrite index to get all the students details
    
    * Overwrite create to create new student to database
    
    * Define the parameters to be accepted
    
    * Add routes to access Student controller

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,27 @@
+class StudentsController < ApplicationController
+  
+  def index
+    @students = Student.select("id","student_name","year","roll_no","batch")
+    if @students.present?
+      render status: 200, json: @students
+    else
+      render status: 200, json: {msg: "No students data present. Use /create_student"}
+    end
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save!
+      render json: 'New Student has been Added!'
+    else
+      render status: 400 , json: {error: "Unable to add new student"}
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:id,:student_name,:year,:roll_no,:batch)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/create_student", to: 'students#create'
+  get "/get_students", to: 'students#index'
+
 end
