342cc7ee331df42f648ffd68d83a1e116b151ef7
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-10 11:28:30 +0530

    Migration to create teachers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_10_053157) do
+ActiveRecord::Schema.define(version: 2022_09_10_054947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2022_09_10_053157) do
     t.string "class"
     t.string "roll_no"
     t.string "batch"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "teacher_id"
+    t.string "teacher_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
